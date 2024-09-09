a6240b184e0f863cf3dbaf07df3f92d454107014
Alef oliveira <alefolivfeira@gmail.com>
2024-09-08 22:21:31 -0300

    feat(backend): add migration rural producert

---
+++ backend/prisma/migrations/20240909012046_add_user_ruralproducers_relation/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "RuralProducers" (
+    "id" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "producerName" TEXT NOT NULL,
+    "farmNAme" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "areaInHectares" DOUBLE PRECISION NOT NULL,
+    "arableInHectares" DOUBLE PRECISION NOT NULL,
+    "vegetationAreaInHectares" DOUBLE PRECISION NOT NULL,
+    "planedCrops" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "usersId" TEXT,
+
+    CONSTRAINT "RuralProducers_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "RuralProducers" ADD CONSTRAINT "RuralProducers_usersId_fkey" FOREIGN KEY ("usersId") REFERENCES "Users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
